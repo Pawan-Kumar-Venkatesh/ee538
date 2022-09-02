d581b4b401f105c7885f446f1fad210c74d5230b
Pawan-Kumar-Venkatesh <pawan13995@gmail.com>
2022-09-02 15:04:50 -0700

    changing test

--- lectures/find_max/find_max_test.cc
+++ lectures/find_max/find_max_test.cc
@@ -1,20 +1,20 @@
 #include "find_max.h"
 #include "gtest/gtest.h"
 
-TEST(FindMax, Test1){
+TEST(FindMax, WorksForASmallSizeVectorOfPositiveNumbers){
     std::vector<int> input={1, 2, 100, 4, 5};
     int expected = 100;
     EXPECT_EQ(FindMax(input), expected);
 }
 
-TEST(FindMax, Test2){
+TEST(FindMax, WorksForASinglElementVector){
     std::vector<int> input={1};
     int expected = 1;
     EXPECT_EQ(FindMax(input), expected);
 }
 
-TEST(FindMax, Test3){
-    std::vector<int> input={-1};
+TEST(FindMax,WorksIfThereAreNoInputs){
+    std::vector<int> input={};
     int expected = -1;
     EXPECT_EQ(FindMax(input), expected);
 }
